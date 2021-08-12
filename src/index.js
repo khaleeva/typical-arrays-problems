32bef4dd54210df03576ef2e45204bfe83d356b6
Eugenia Khaleeva <khaleeva20990@gmail.com>
2021-08-12 18:23:59 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
+  
+  if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){ 
+    
+   let minNumber = Math.min.apply(null, array);
+   return minNumber; 
+        
+      }  
+        return 0;
+  }
+     
+ 
 exports.max = function max (array) {
-  return 0;
-}
+
+  
+  if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){ 
+    
+    let maxNumber = Math.max.apply(null, array);
+    return maxNumber; 
+         
+       }  
+         return 0;
+   }
+  
+
 
 exports.avg = function avg (array) {
+  if (typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+    let avgSum = array.reduce((a,b) => a+b);
+      return avgSum / array.length;
+  }
+
   return 0;
+
+  
 }
+
+
